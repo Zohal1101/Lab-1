bc1990b41b9e73f5d33d5e556f9ad9fa26af8c12
sinasara <sara.sina@mail.utoronto.ca>
2025-05-15 21:02:01 -0400

    Task 3.3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,15 +4,14 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
-
         int i = 1;
         while (i < 100) {
             i = doFizzBuzz(i);
+            i += 1;
         }
     }
 
-    private static int doFizzBuzz(int i) {
-        i += 1;
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
