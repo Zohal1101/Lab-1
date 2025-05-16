ffaa233b56e55fe5ad50fdaff974f8a3d9761cf1
sinasara <sara.sina@mail.utoronto.ca>
2025-05-15 21:03:14 -0400

    Task 3.3 (final)

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,7 +6,7 @@ class FizzBuzz {
     public static void main(String[] args) {
         int i = 1;
         while (i < 100) {
-            i = doFizzBuzz(i);
+            doFizzBuzz(i);
             i += 1;
         }
     }
